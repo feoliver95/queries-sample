eca73b1b6e9a4fe6eec56853db4a56a56ce32530
feoliver95 <48654311+feoliver95@users.noreply.github.com>
2021-04-14 15:53:09 -0300

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,7 @@
+#com *
+select * from `bigquery-public-data.github_repos.commits` ;
+
+#selecionando colunas
+
+select author,author.name,author.email   from `bigquery-public-data.github_repos.commits` ;
+
