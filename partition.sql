25b44e8eb5b2d480849752baff377f3c596c46fc
feoliver95 <48654311+feoliver95@users.noreply.github.com>
2021-04-23 08:23:36 -0300

    Update partition.sql

--- partition.sql
+++ partition.sql
@@ -1,9 +1,9 @@
 #sem consultar pela partição
 
-SELECT * FROM `interoper-dataplatform-prd.raw_motion.motion_tb_requisicao` ;
+SELECT * FROM `interoper-dataplatform-prd.temp_treinamento.motion_tb_requisicao` ;
 
 #consultando pela particao
 
-SELECT * FROM `interoper-dataplatform-prd.raw_motion.motion_tb_requisicao` 
+SELECT * FROM `interoper-dataplatform-prd.temp_treinamento.motion_tb_requisicao` 
 WHERE dt = "2021-04-04" 
 
